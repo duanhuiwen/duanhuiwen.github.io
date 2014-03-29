65baf342a00f52a3f8b29cebbd12735d172e02e6
duanhuiwen <duanhuiwen@gmail.com>
2014-03-29 23:56:19 +0200

    menu worked

--- js/controllers.js
+++ js/controllers.js
@@ -1,18 +1,31 @@
 var siteController = angular.module('siteController', []);
 
 
-siteController.controller('NavController',['$scope',function($scope){
-$scope.menu = ["About", "Work", "Contact"];
+siteController.controller('NavController',['$scope','$routeParams',function($scope,$routeParams){
+$scope.menu = ["HUIWEN","About", "Work", "Contact"];
 
   $scope.selectedIndex = 0; // Whatever the default selected index is, use -1 for no selection
-
+  
+  
   $scope.itemClicked = function ($index) {
     $scope.selectedIndex = $index;
+    $scope.selectedItem = $scope.menu[$index];
+    console.log($scope.selectedItem);
   };
 
 
 
 }]) ;
+
+siteController.controller('AboutController', ['$scope', '$http',
+  function ($scope, $http) {
+    $http.get('phones/phones.json').success(function(data) {
+      $scope.phones = data;
+    });
+ 
+    $scope.orderProp = 'age';
+  }]);
+
 
 siteController.controller('WorkController', ['$scope', '$http',
   function ($scope, $http) {
